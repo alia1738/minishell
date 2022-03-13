c5bf31405a2b6c3ec7a75f35fa7cde3852950720
Alia Salem <Alia@Alias-MacBook.local>
2022-03-13 11:02:04 +0400

    bultin_check function

--- command_execution.c
+++ command_execution.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   command_execution.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anasr <anasr@student.42.fr>                +#+  +:+       +#+        */
+/*   By: Alia <Alia@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 18:48:43 by aalsuwai          #+#    #+#             */
-/*   Updated: 2022/03/04 14:18:59 by anasr            ###   ########.fr       */
+/*   Updated: 2022/03/12 15:22:52 by Alia             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,33 @@ static void	child_job(t_parser_info *p)
 	exit(127);
 }
 
+int	bultin_check(t_parser_info *p)
+{
+	if (!ft_strncmp(p->cmd[0], "echo", 5))
+		;
+	else if (!ft_strncmp(p->cmd[0], "cd", 3))
+		;
+	else if (!ft_strncmp(p->cmd[0], "pwd", 4))
+		;
+	else if (!ft_strncmp(p->cmd[0], "export", 7))
+		;
+	else if (!ft_strncmp(p->cmd[0], "unset", 7))
+		;
+	else if (!ft_strncmp(p->cmd[0], "env", 4))
+		;
+	return(1);
+}
+
 void	execute_command(t_parser_info *p)
 {
 	pid_t	child;
 
-	child = fork();
-	if (!child)
-		child_job(p);
-	else
-		waitpid(-1, 0, 0);
+	if (builtin_check(p)) // builtin return 0 
+	{
+		child = fork();
+		if (!child)
+			child_job(p);
+		else
+			waitpid(-1, 0, 0);
+	}
 }
